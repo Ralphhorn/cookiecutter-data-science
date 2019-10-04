69dc5cf2225795cc7bbd5a8e5ebdf6e269fee093
Ralph Horn <mail@rshorn.nl>
2019-10-04 17:21:05 +0200

    add post_gen_hook for removing docker folder

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+print(os.getcwd())  
+
+def remove(filepath):
+    if os.path.isfile(filepath):
+        os.remove(filepath)
+    elif os.path.isdir(filepath):
+        shutil.rmtree(filepath)
+
+
+create_docker_folder = '{{cookiecutter.docker_container}}' != 'None'
+
+if not create_docker_folder:
+     # remove absolute path to file nested inside the generated folder
+    remove(os.path.join(os.getcwd(),'docker'))
+    print('removed docker folder')
+
+
