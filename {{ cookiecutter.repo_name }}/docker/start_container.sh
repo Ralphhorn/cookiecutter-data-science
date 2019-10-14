3ad22a4af8cb3197cc7944af21451c05ddbf9e27
Ralph Horn <mail@rshorn.nl>
2019-10-14 13:00:33 +0200

    fixed docker start script to work with non pytorch containers

--- {{ cookiecutter.repo_name }}/docker/start_container.sh
+++ {{ cookiecutter.repo_name }}/docker/start_container.sh
@@ -5,5 +5,5 @@ source $mydir/../.env
 [[ $(docker ps -f "name=$DOCKER_NAME" --format '{{.Names}}') == $DOCKER_NAME ]] || \
 docker run -d -it --rm --shm-size=1g --name $DOCKER_NAME -p 8888:8888 -p 6006:6006 \
 -v $WORKSPACE_ROOT:/workspace/ $DOCKER_NAME /bin/bash \
--c "pip install --editable . && jupyter lab --ip=0.0.0.0 --notebook-dir=/workspace --allow-root --no-browser --NotebookApp.token='$DOCKER_PASSWORD'"
+-c "pip install --editable /workspace/ && jupyter lab --ip=0.0.0.0 --notebook-dir=/workspace --allow-root --no-browser --NotebookApp.token='$DOCKER_PASSWORD'"
 {% endraw %}
