de165b58f48289094f219687be9a6571dc341c9c
Ralph Horn <mail@rshorn.nl>
2019-10-14 19:37:41 +0200

    docker container fixes

--- {{ cookiecutter.repo_name }}/docker/start_container.sh
+++ {{ cookiecutter.repo_name }}/docker/start_container.sh
@@ -3,7 +3,7 @@ mydir=$(dirname "$0")
 source $mydir/../.env
 
 [[ $(docker ps -f "name=$DOCKER_NAME" --format '{{.Names}}') == $DOCKER_NAME ]] || \
-docker run -d -it --rm --shm-size=1g --name $DOCKER_NAME --user $(id -u):$(id -g) -p 8888:8888 -p 6006:6006 \
+docker run -d -it --rm --shm-size=1g --name $DOCKER_NAME  -p 8888:8888 -p 6006:6006 \
 -v $WORKSPACE_ROOT:/workspace/ $DOCKER_NAME /bin/bash \
--c "pip install --editable /workspace/ && jupyter lab --ip=0.0.0.0 --notebook-dir=/workspace --allow-root --no-browser --NotebookApp.token='$DOCKER_PASSWORD'"
+-c "pip install --editable /workspace/ && jupyter-notebook --ip=0.0.0.0 --notebook-dir=/workspace --allow-root --no-browser --NotebookApp.token='$DOCKER_PASSWORD'"
 {% endraw %}
